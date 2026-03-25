34fec365403f21ca28e4620698f4897496291ed0
Guillermo Angeles <guilleangeles529@gmail.com>
2026-03-25 15:01:37 -0400

    anadiendo el juego de las fechas

---
+++ python projects/BIRTHDAY_PARADOX.py
@@ -0,0 +1,37 @@
+import random
+
+print("Birthday Paradox")
+print("\nHow many birthdays shall I generate?")
+people = int(input("> "))
+
+birthdays = []
+seen = set()
+repetidos = []
+
+MONTHS = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+
+days_in_month = (31, 28, 31, 30, 31, 30,
+                 31, 31, 30, 31, 30, 31)
+
+for i in range(people):
+    month = random.randint(1, 12)
+    day = random.randint(1, days_in_month[month - 1])
+
+    birthday = (month, day)
+    birthdays.append(birthday)
+
+    if birthday in seen:
+        repetidos.append(birthday)
+    else:
+        seen.add(birthday)
+
+print("\nBirthdays generated:")
+for b in birthdays:
+    print(f"{MONTHS[b[0]-1]} {b[1]}")
+
+if len(repetidos) > 0:
+    print("\nThere is at least one matching birthday!")
+    print("Repeated birthdays:", repetidos)
+else:
+    print("\nNo matching birthdays.")
